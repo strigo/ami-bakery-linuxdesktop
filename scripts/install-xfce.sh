08bbc5a1437a2a88a5692cbe91f4ae5fff5c6e60
Michael Sverdlik <michael@strigo.io>
2024-03-31 19:34:47 +1100

    Add xfce4 config to address default config problems
    - disable lock screen, screen saver and display power managment
    - fix polkit auth popup request on first login

--- scripts/install-xfce.sh
+++ scripts/install-xfce.sh
@@ -9,3 +9,40 @@ export XDG_DATA_DIRS=/usr/share/xfce4:/usr/share/xubuntu:/usr/local/share:/usr/s
 export XDG_CONFIG_DIRS=/etc/xdg/xdg-xubuntu:/etc/xdg:/etc/xdg
 EOF
 chown ubuntu:ubuntu /home/ubuntu/.xsessionrc /home/ubuntu/.xsession
+
+# disable screen saver, screen lock and display power managment
+cat << EOF > /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-screensaver.xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<channel name="xfce4-screensaver" version="1.0">
+  <property name="lock" type="empty">
+    <property name="enabled" type="bool" value="false"/>
+  </property>
+  <property name="saver" type="empty">
+    <property name="enabled" type="bool" value="false"/>
+  </property>
+</channel>
+EOF
+
+cat << EOF > /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<channel name="xfce4-power-manager" version="1.0">
+  <property name="xfce4-power-manager" type="empty">
+    <property name="power-button-action" type="empty"/>
+    <property name="show-tray-icon" type="bool" value="false"/>
+    <property name="dpms-enabled" type="bool" value="false"/>
+  </property>
+</channel>
+EOF
+
+# adjust polkit config to stop displaying the
+# "Authentication Required to Create Managed Color Device" popup message at login
+cat << EOF >  /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
